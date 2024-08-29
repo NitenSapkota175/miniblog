f2d325000996112db2d1142148188a69a0f60a40
NitenSapkota175 <nitensapkota1705@gmail.com>
2024-08-30 03:09:35 +0530

    views and urls.py created for rendering all the html pages

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 urlpatterns = [
     path('',views.Home,name="Home"),
-      path('about/',views.About,name="About")
+    path('about/',views.About,name="About"),
+    path('contact/',views.Contact,name="Contact"),
+    path('dashboard/',views.Dashboard,name="Dashboard"),
+    path('login/',views.UserLogin,name="Login"),
+    path('signup/',views.Signup,name="Signup"),
+    path('logout/',views.UserLogout,name="Logout"),
 ]
